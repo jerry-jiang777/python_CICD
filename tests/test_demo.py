c7439cb7780a08666936a134b046524a52e59b18
JerryJiang <jgx518@163.com>
2025-07-23 15:24:44 +0800

    配置飞书/邮箱集成通知

---
+++ tests/test_demo.py
@@ -0,0 +1,12 @@
+import allure
+
+@allure.epic("CI流程测试")
+@allure.feature("基础功能")
+@allure.story("加法功能")
+@allure.title("测试加法是否正确")
+def test_add():
+    assert 1 + 1 == 2
+
+@allure.title("模拟失败的用例")
+def test_fail():
+    assert 2 + 2 == 5  # 故意失败用例
