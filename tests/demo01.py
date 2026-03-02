2cd586744185f0968d31cb9b972ccfa14d5cd99c
guixuejiang <jgx518@163.com>
2026-03-02 16:54:23 +0800

    提交修改的代码

--- tests/demo01.py
+++ tests/demo01.py
@@ -3,8 +3,14 @@ def add(a, b):
     return result
 
 
+def subtract(a, b):
+    result = a - b
+    return result
 
 
 if __name__ == "__main__":
     res = add(1, 2)
     print(f"res = {res}")
+    
+    res_subtract = subtract(5, 3)
+    print(f"res_subtract = {res_subtract}")
